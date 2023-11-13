9162015a62fc1cd276bb68807e12affcdb064785
olliebalabil <oliverbalabil@gmail.com>
2023-11-13 12:44:35 +0000

    added pages

---
+++ client/src/pages/CreatePage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import {CreateArtwork} from "../../components"
+
+export default function CreatePage(){
+  return (
+    <>
+    <CreateArtwork/>
+    </>
+  )
+}
---
+++ client/src/pages/RegisterPage/index.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react"
+
+export default function Register() {
+  return (
+    <>
+      Register
+    </>
+  )
+}
