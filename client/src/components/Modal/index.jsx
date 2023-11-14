71ab2f6282606905e66698cabe47ab283db21388
olliebalabil <oliverbalabil@gmail.com>
2023-11-14 12:53:55 +0000

    closing modal

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 const Modal = ({ isOpen, onClose,closeModal, children }) => {
   if (!isOpen) return null;
 
+  function handleClick(e) {
+    if(e.target.className=='modal-content'){
+      closeModal()
+    }
+  }
+
   return (
     <div className="modal is-open">
-      <div className="modal-content">
-        <span class="close" onClick={closeModal}>&times;</span>
+      <div className="modal-content" onClick={handleClick}>
+        <span className="close" onClick={closeModal}>&times;</span>
         {children}
       </div>
     </div>
