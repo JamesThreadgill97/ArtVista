35b22a61ffe0273db69af47ffde23b1988724fc8
olliebalabil <oliverbalabil@gmail.com>
2023-11-14 14:25:45 +0000

    minor changes

--- client/src/pages/ArtworkPage/index.jsx
+++ client/src/pages/ArtworkPage/index.jsx
@@ -26,13 +26,13 @@ export default function ArtworkPage() {
           <img src="../../../assets/20220409132925_IMG_3548~2.JPG" alt={data.title} onClick={openModal} />
           <Modal className="modal" isOpen={isModalOpen} closeModal={closeModal}>
             <div className="modal-image-div">
-              <img src="../../../assets/cat.JPG" alt={data.title} className="modal-image" />
+              <img src="../../../assets/20220409132925_IMG_3548~2.JPG" alt={data.title} className="modal-image" />
             </div>
           </Modal>
         </div>
         <div className="artwork-info">
           <h1>{data.title}</h1>
-          <ProfileLink />
+          <ProfileLink id={id} />
           <h3>TAGS HERE</h3>
           <p>{data.description}</p>
           <Comment />
