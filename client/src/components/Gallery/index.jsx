181f0b53e66299bf064e97ab634efa4ebbc06d0b
olliebalabil <oliverbalabil@gmail.com>
2023-11-15 11:05:14 +0000

    minor changes

--- client/src/components/Gallery/index.jsx
+++ client/src/components/Gallery/index.jsx
@@ -1,29 +1,7 @@
-import React, {useState,useEffect} from 'react'
+import React from 'react'
 import ArtworkCard from "../ArtworkCard"
 
-export default function Gallery() {
-  const [artworks,setArtworks] = useState([])
-  //mocking data from fetch
-  let arr = [];
-  for (let i = 1; i<50;i++){
-    arr.push({
-      id: i,
-      title: "title" + i,
-      description: "description" + i,
-      likes: 0
-    })
-  }
-
-  //gets all images
-  useEffect(()=>{
-    const fetchArtworks = async () => {
-      const response = await fetch("https://artvista-api.onrender.com/art/")
-      const data = await response.json()
-      console.log(data)
-      setArtworks(data)
-    }
-    fetchArtworks()
-  },[])
+export default function Gallery({artworks}) {
 
   return (
     <div className='gallery'>
