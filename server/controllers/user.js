daea95795df941b62946920a15ed089d4d88fbbf
olliebalabil <oliverbalabil@gmail.com>
2023-11-15 15:19:57 +0000

    user_id also returned when logged in

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -33,7 +33,7 @@ async function login(req, res) {
       throw new Error('Incorrect credentials.');
     } else {
       const token = await Token.create(user.id);
-      res.status(200).json({ authenticated: true, token: token.token });
+      res.status(200).json({ authenticated: true, token: token.token, user_id: token.user_id });
     }
   } catch (err) {
     res.status(403).json({ error: err.message });
