e09a420c1912d459e43d7568cbd08244b5b6bbae
olliebalabil <oliverbalabil@gmail.com>
2023-11-15 10:21:29 +0000

    fetches imgs for gallery and artworkpage

--- client/src/pages/ArtworkPage/index.jsx
+++ client/src/pages/ArtworkPage/index.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-import { Gallery, ProfileLink, Comment, CommentForm, Modal } from "../../components"
+import { Gallery, ProfileLink, Comments, CommentForm, Modal } from "../../components"
 
 export default function ArtworkPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [comments,setComments] =useState("")
+  const [artwork,setArtwork] = useState({})
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -13,30 +15,41 @@ export default function ArtworkPage() {
     setIsModalOpen(false);
   };
   const { id } = useParams()
-  const data = {
-    id: id,
-    title: "title" + id,
-    description: "description" + id,
-    likes: 0
-  }
+
+  useEffect(()=> {
+    const fetchArtwork = async () => {
+      try {
+        const response = await fetch(`https://artvista-api.onrender.com/art/${id}`)
+        const data = await response.json()
+        if (response.status == 200) {
+          setArtwork(data)
+        }
+      }
+      catch (err) {
+        console.error({error: err.message})
+      }
+    }
+    fetchArtwork()
+  },[])
+
   return (
     <>
       <div className="artwork-and-info">
         <div className="displayed-artwork">
-          <img src="../../../assets/20220409132925_IMG_3548~2.JPG" alt={data.title} onClick={openModal} />
+          <img src={artwork.url} alt={artwork.title} onClick={openModal} />
           <Modal className="modal" isOpen={isModalOpen} closeModal={closeModal}>
             <div className="modal-image-div">
-              <img src="../../../assets/20220409132925_IMG_3548~2.JPG" alt={data.title} className="modal-image" />
+              <img src={artwork.url} alt={artwork.title} className="modal-image" />
             </div>
           </Modal>
         </div>
         <div className="artwork-info">
-          <h1>{data.title}</h1>
-          <ProfileLink id={id} />
+          <h1>{artwork.title}</h1>
+          <ProfileLink id={artwork.user_id} />
           <h3>TAGS HERE</h3>
-          <p>{data.description}</p>
-          <Comment />
-          <CommentForm />
+          <p>{artwork.description}</p>
+          <Comments comments={comments} id={id}/>
+          <CommentForm setComments={setComments} id={id}/>
         </div>
       </div>
       <Gallery />
