1cd931718f986e089cbbf2c83b324da60b8b84be
olliebalabil <oliverbalabil@gmail.com>
2023-11-16 10:04:16 +0000

    added getUserInfo to backend

--- server/routers/user.js
+++ server/routers/user.js
@@ -7,5 +7,6 @@ const userRouter = Router();
 userRouter.post('/register', userController.register);
 userRouter.post('/login', userController.login);
 userRouter.get('/showId', userController.getUserId);
+userRouter.get("/:id", userController.getUserInfo); //gets user's username (and profile pic?) with id. Allows user's to see other users' username in comment section, profile section, etc.
 
 module.exports = userRouter;
