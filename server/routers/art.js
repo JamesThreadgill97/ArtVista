ec13b451324e252f548bcc29b22218615fb84030
olliebalabil <140207585+olliebalabil@users.noreply.github.com>
2023-11-17 14:35:55 +0000

    Update art.js

--- server/routers/art.js
+++ server/routers/art.js
@@ -9,9 +9,9 @@ const upload = multer(); // Initialize Multer
 artRouter.get('/', artController.index);
 artRouter.get('/:id', artController.show);
 artRouter.get('/:id/comments', artController.comments);
-artRouter.get('/:art_id/:user_id', authenticator, artController.likes)
-artRouter.post('/:art_id/:user_id', authenticator, artController.postLike)
-artRouter.delete('/:art_id/:user_id', authenticator, artController.destroyLike)
+artRouter.get('/like/:art_id/:user_id', authenticator, artController.likes)
+artRouter.post('/like/:art_id/:user_id', authenticator, artController.postLike)
+artRouter.delete('/like/:art_id/:user_id', authenticator, artController.destroyLike)
 artRouter.post('/', upload.single('file'), authenticator, artController.create); // Use Multer middleware for file upload
 artRouter.patch('/:id', authenticator, artController.update);
 artRouter.delete('/:id', authenticator, artController.destroy);
