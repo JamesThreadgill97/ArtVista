704ee9d4ce399674ab0924dc5dbec2d8f31060b5
Kieran <kieranhull@hotmail.co.uk>
2023-11-17 14:43:57 +0000

    fixes

--- server/routers/art.js
+++ server/routers/art.js
@@ -12,7 +12,7 @@ artRouter.get('/:id/comments', artController.comments);
 artRouter.get('/:art_id/:user_id', authenticator, artController.likes)
 artRouter.post('/:art_id/:user_id', authenticator, artController.postLike)
 artRouter.delete('/:art_id/:user_id', authenticator, artController.destroyLike)
-artRouter.post('/', upload.single('file'), authenticator, artController.create); // Use Multer middleware for file upload
+artRouter.post('/', [upload.single('file'), authenticator], artController.create); // Use Multer middleware for file upload
 artRouter.patch('/:id', authenticator, artController.update);
 artRouter.delete('/:id', authenticator, artController.destroy);
 artRouter.get('/tags/:id', artController.showTags) //gets tags for specific artwork
