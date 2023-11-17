898c433be2283355de00cbcdd7396d2587ca4d3b
Kieran <kieranhull@hotmail.co.uk>
2023-11-17 14:11:14 +0000

    auth added (please dont break)

--- server/routers/art.js
+++ server/routers/art.js
@@ -9,12 +9,12 @@ const upload = multer(); // Initialize Multer
 artRouter.get('/', artController.index);
 artRouter.get('/:id', artController.show);
 artRouter.get('/:id/comments', artController.comments);
-artRouter.get('/:art_id/:user_id', artController.likes)
-artRouter.post('/:art_id/:user_id', artController.postLike)
-artRouter.delete('/:art_id/:user_id', artController.destroyLike)
-artRouter.post('/', upload.single('file'), artController.create); // Use Multer middleware for file upload
-artRouter.patch('/:id', artController.update);
-artRouter.delete('/:id', artController.destroy);
+artRouter.get('/:art_id/:user_id', authenticator, artController.likes)
+artRouter.post('/:art_id/:user_id', authenticator, artController.postLike)
+artRouter.delete('/:art_id/:user_id', authenticator, artController.destroyLike)
+artRouter.post('/', upload.single('file'), authenticator, artController.create); // Use Multer middleware for file upload
+artRouter.patch('/:id', authenticator, artController.update);
+artRouter.delete('/:id', authenticator, artController.destroy);
 artRouter.get('/tags/:id', artController.showTags) //gets tags for specific artwork
 
 module.exports = artRouter;
