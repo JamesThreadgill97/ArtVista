596bc37aabd2997f65fbca1386e8696972d050c4
Kieran <kieranhull@hotmail.co.uk>
2023-11-22 10:47:00 +0000

    fixes

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-
+const authenticator = require('../middleware/authenticator')
 const userController = require('../controllers/user');
 
 const userRouter = Router();
