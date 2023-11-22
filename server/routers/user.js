a46c1f5df894cef3415befd6643e714d0f355f5c
Kieran <kieranhull@hotmail.co.uk>
2023-11-22 10:31:37 +0000

    auth readded

--- server/routers/user.js
+++ server/routers/user.js
@@ -8,5 +8,5 @@ userRouter.post('/register', userController.register);
 userRouter.post('/login', userController.login);
 userRouter.get('/showId', userController.getUserId);
 userRouter.get("/userInfo/:id", userController.getUserInfo); //gets user's username (and profile pic?) with id. Allows user's to see other users' username in comment section, profile section, etc.
-userRouter.patch('/update', userController.update)
+userRouter.patch('/update', authenticator, userController.update)
 module.exports = userRouter;
