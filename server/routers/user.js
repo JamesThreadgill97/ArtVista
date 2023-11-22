bf8b0941a6f5b684d0422af616cfe412160c2e2e
olliebalabil <140207585+olliebalabil@users.noreply.github.com>
2023-11-22 15:26:46 +0000

    Fixing bug in route

--- server/routers/user.js
+++ server/routers/user.js
@@ -9,5 +9,5 @@ userRouter.post('/register', upload.single('file'), userController.register);
 userRouter.post('/login', userController.login);
 userRouter.get('/showId', userController.getUserId);
 userRouter.get("/userInfo/:id", userController.getUserInfo); //gets user's username (and profile pic?) with id. Allows user's to see other users' username in comment section, profile section, etc.
-userRouter.patch('/update', authenticator, userController.update)
+userRouter.patch('/update/:id', authenticator, userController.update)
 module.exports = userRouter;
