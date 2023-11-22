bbec3df32a97b71d7e62bba6e838248ec2083943
olliebalabil <oliverbalabil@gmail.com>
2023-11-22 12:47:45 +0000

    profile

--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,47 +1,57 @@
 import React, { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { Gallery } from "../../components"
+import pen from "../../../assets/pen.png"
 
 export default function ProfilePage() {
   const { id } = useParams()
-  const [artworks,setArtworks] = useState([])
-  const [userInfo,setUserInfo] = useState({})
+  const [artworks, setArtworks] = useState([])
+  const [userInfo, setUserInfo] = useState({})
 
-  useEffect(()=>{
+  useEffect(() => {
     const fetchArtworks = async () => {
-      try{
+      try {
         const response = await fetch("https://artvista-api.onrender.com/art/")
         const data = await response.json()
         if (response.status == 200)
-        setArtworks(data.filter((el) => el.user_id == id))
+          setArtworks(data.filter((el) => el.user_id == id))
       } catch (err) {
-        console.error({error:err.message})
+        console.error({ error: err.message })
       }
     }
     fetchArtworks()
-  },[])
-  useEffect(()=>{
+  }, [])
+  useEffect(() => {
     const fetchUserDataById = async () => {
       try {
-          const response = await fetch(`https://artvista-api.onrender.com/users/userInfo/${id}`)
-          const data = await response.json()
-          if (response.status == 200) {
-            setUserInfo(data)
-          }
+        const response = await fetch(`https://artvista-api.onrender.com/users/userInfo/${id}`)
+        const data = await response.json()
+        if (response.status == 200) {
+          setUserInfo(data)
+        }
       } catch (err) {
-        console.error({error:err.message})
+        console.error({ error: err.message })
       }
     }
     fetchUserDataById()
-  },[])
+  }, [])
   return (
-    <>  
-      <div>
-        <img className="profile-page-pic" src={userInfo.profile_url} alt="Profile Pic" />
-        <h1>{userInfo.username}</h1>
+    <>
+      <div className="profile-page">
+        <div className="profile-page-info">
+          <div className="edit-button">
+          <img src={pen} alt="edit pen" />
+          </div>
+          <img className="profile-page-pic" src={userInfo.profile_url} alt="Profile Pic" />
+          <h1>{userInfo.username}</h1>
+          <h3>Contact: <a href="mailto:www.google.com">ollie235@gmail.com</a></h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nulla risus. Aliquam mattis, lectus at molestie consequat, diam metus pulvinar mauris.</p>
+        </div>
+        <div className="profile-page-artwork">
+          {artworks.length == 0 && <h2>{userInfo.username}'s gallery is empty.</h2>}
+          <Gallery className="profile-gallery" artworks={artworks} />
+        </div>
       </div>
-        {artworks.length == 0 && <h2>{userInfo.username}'s gallery is empty.</h2>}
-        <Gallery artworks={artworks}/>
     </>
   )
 }
